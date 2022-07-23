3e46ce755c0217707d38ba23e92ee93618c163f1
Sasha K <alexandra.kolchedantseva@cluepoints.com>
2022-07-23 11:14:44 +0200

    liquidity

--- frontend/src/lib/components/market/Market.svelte
+++ frontend/src/lib/components/market/Market.svelte
@@ -1,51 +1,63 @@
 <script lang="ts">
 	import { getMarket } from '$lib/utils/market.store';
-import Trade from './Trade.svelte';
+	import Liquidity from './Liquidity.svelte';
+	import Trade from './Trade.svelte';
 	export let contractAddress: string;
 
 	const market = getMarket(contractAddress);
 
-  enum View {
-    trade, liquidity
-  }
+	enum View {
+		trade,
+		liquidity
+	}
 
 	let view: View = View.trade;
 
-  function setView(_view: View) {
-    view = _view;
+	function setView(_view: View) {
+		view = _view;
 		return null;
-  }
-
-
+	}
 </script>
 
 {#if $market}
 	<div class="center">
-	<h2 class="text-center">{$market.title}</h2>
-	<h4 class="text-center">{$market.description}</h4>
-	<div class="el">
-		<div class="toggle-group">
-			<div type="button" class="btn btn-lg toggle-btn me-3" class:selected={view === View.trade} on:click="{setView(View.trade)}">Buy / Sell</div>
-			<div type="button" class="btn btn-lg toggle-btn ms-3" class:selected={view === View.liquidity} on:click="{setView(View.liquidity)}">Liquidity</div>
+		<h2 class="text-center">{$market.title}</h2>
+		<h4 class="text-center">{$market.description}</h4>
+		<div class="el">
+			<div class="toggle-group">
+				<div
+					type="button"
+					class="btn btn-lg toggle-btn me-3"
+					class:selected={view === View.trade}
+					on:click={setView(View.trade)}
+				>
+					Buy / Sell
+				</div>
+				<div
+					type="button"
+					class="btn btn-lg toggle-btn ms-3"
+					class:selected={view === View.liquidity}
+					on:click={setView(View.liquidity)}
+				>
+					Liquidity
+				</div>
+			</div>
+		</div>
+		<div class="el">
+			
+				{#if view === View.trade}
+					<Trade market={$market} />
+				{/if}
+				{#if view === View.liquidity}
+					<Liquidity market={$market} />
+				{/if}
+			
 		</div>
 	</div>
-	<div class="el">
-		<div class="card-fancy">
-			{#if view === View.trade}
-				<Trade market={$market}></Trade>
-				
-			{/if}
-		</div>
-	</div>
-</div>
 {/if}
-
-
 
 <style lang="scss">
 	.center {
-		
-
 		.el {
 			display: flex;
 			justify-content: center;
